d5d192f0ad900a95ffe29ceea9385d07cad7fa39
LEWY Nathan <nathan.lewy@soprasteria.com>
2025-09-11 17:44:30 +0200

    idk

--- t5inference.py
+++ t5inference.py
@@ -1,10 +1,13 @@
 import torch
 from transformers import pipeline
-
+import os
 pipeline = pipeline(
     task="text2text-generation",
     model="google-t5/t5-base",
     dtype=torch.float16,
     device=0
 )
-print(pipeline("translate English to French: The weather is nice today."))
+file =  './outputs/output_0.txt'
+with open(file, 'r', encoding='utf-8') as txt:
+    prompt = 'Trouve le montant à payer TTC pour le 05/25 dans cette facture : \n DEBUT DU DOCUMENT \n' + txt.read() + '\n FIN DU DOCUMENT \n Le montant à payer TTC pour le 05/25 dans cette facture est donc de :'
+    print(pipeline(prompt))
